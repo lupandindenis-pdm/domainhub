53ff82691e94055910a027f90de05d9e050aa5b2
Denis <lupandindenis@gmail.com>
2026-02-06 00:38:08 +0800

    fix: удалены GLOBAL, APAC и INTERNAL из geo - заменены на WW

--- src/components/domains/GeoMultiSelector.tsx
+++ src/components/domains/GeoMultiSelector.tsx
@@ -10,7 +10,6 @@ import { getGeoColor } from "@/data/geoColors";
 
 // Список всех стран с кодами
 const geoOptions = [
-  { code: "GLOBAL", name: "Global" },
   { code: "WW", name: "Worldwide" },
   { code: "RU", name: "Россия" },
   { code: "US", name: "США" },
